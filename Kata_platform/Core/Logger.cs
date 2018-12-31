77f28666e4d44e2fce511b9313ebc70ce9d03706
Roman <romanshcherbich@gmail.com>
2018-12-31 09:57:36 +0300

    VS solution Kata_platform

---
+++ Kata_platform/Core/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Kata_platform.Core
+{
+    class Logger
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine("INFO: {0}", message);
+        }
+        public void Info(string message, params object[] args)
+        {
+            var info = string.Format(message, args);
+            Console.WriteLine("INFO: {0}", info);
+    }
+    }
+}
